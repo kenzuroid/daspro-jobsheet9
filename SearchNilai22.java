949f2408b6279ad230ccda18f696e7e2172f0541
kenzuroid <sholihahsitinikmatus@gmail.com>
2024-10-30 18:43:39 +0700

    perubahan3

--- SearchNilai22.java
+++ SearchNilai22.java
@@ -1,17 +1,35 @@
+import java.util.Scanner;
+
 public class SearchNilai22 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlahElemen = sc.nextInt();
+        
+        int[] arrNilai = new int[jumlahElemen]; 
+
+        for (int i = 0; i < jumlahElemen; i++) {
+            System.out.print("Masukkan nilai ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        int hasil = -1; 
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
-                break;
+                hasil = i; 
+                break; 
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+ key +" ketemu di indeks ke-" + hasil);
-        System.out.println();
+
+        if (hasil != -1) {
+            System.out.println();
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan.");
+        }
     }
 }
